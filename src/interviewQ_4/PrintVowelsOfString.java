dda279a4345ac7415135da7a6b5723d8d86fe840
murat akdeniz <akdenizmurat5205@gmail.com>
2022-04-16 02:22:05 +0300

    interview

--- src/interviewQ_4/PrintVowelsOfString.java
+++ src/interviewQ_4/PrintVowelsOfString.java
@@ -6,13 +6,22 @@ public class PrintVowelsOfString {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("yaziniz...");
-        String a = scan.nextLine();
+        String a = scan.nextLine().toLowerCase();
 
 
         for (int i = 0; i <a.length() ; i++) {
-            if (a.charAt(i)=='a'||)
+            if (a.substring(i,i+1).equals("a")||a.substring(i,i+1).equals("e")||
+                    a.substring(i,i+1).equals("i")||a.substring(i,i+1).equals("o")
+                    ||a.substring(i,i+1).equals("u")){
+                System.out.print(a.substring(i,i+1));
 
+            }
         }
+        System.out.println("************");
+
+        String s=a.replaceAll("[^aeiou]","");// [^aeiou] bu karakterlr haric , digerlerini hiclik yap
+        System.out.println(s);
+
 
     }
 }
